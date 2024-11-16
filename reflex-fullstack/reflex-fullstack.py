0fe72f023177a832a1521e00544bf5d786d8ba5d
marcuszou <marcus.zou@gmail.com>
2024-11-16 20:24:53 +0200

    update 30'

--- reflex-fullstack/reflex-fullstack.py
+++ reflex-fullstack/reflex-fullstack.py
@@ -6,21 +6,118 @@ from rxconfig import config
 
 
 class State(rx.State):
-    """The app state."""
 
-    ...
+    label = "Welcome to Reflex!"
 
+    def handle_title_input_change(self, val):
+        self.label = val
+
+def navbar_link(text: str, url: str) -> rx.Component:
+    return rx.link(
+        rx.text(text, size="4", weight="medium"), href=url
+    )
+
+
+def navbar() -> rx.Component:
+    return rx.box(
+        rx.desktop_only(
+            rx.hstack(
+                rx.hstack(
+                    rx.image(
+                        src="/logo.jpg",
+                        width="2.25em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Reflex", size="7", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.hstack(
+                    navbar_link("Home", "/#"),
+                    navbar_link("About", "/#"),
+                    navbar_link("Pricing", "/#"),
+                    navbar_link("Contact", "/#"),
+                    spacing="5",
+                ),
+                rx.hstack(
+                    rx.button(
+                        "Sign Up",
+                        size="3",
+                        variant="outline",
+                    ),
+                    rx.button("Log In", size="3"),
+                    spacing="4",
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        rx.mobile_and_tablet(
+            rx.hstack(
+                rx.hstack(
+                    rx.image(
+                        src="/logo.jpg",
+                        width="2em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Reflex", size="6", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.menu.root(
+                    rx.menu.trigger(
+                        rx.icon("menu", size=30)
+                    ),
+                    rx.menu.content(
+                        rx.menu.item("Home"),
+                        rx.menu.item("About"),
+                        rx.menu.item("Pricing"),
+                        rx.menu.item("Contact"),
+                        rx.menu.separator(),
+                        rx.menu.item("Log in"),
+                        rx.menu.item("Sign up"),
+                    ),
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        bg=rx.color("accent", 3),
+        padding="1em",
+        # position="fixed",
+        # top="0px",
+        # z_index="5",
+        width="100%",
+    )
+
+def base_page(child: rx.Component, *args, **kwargs) -> rx.Component:
+    # print([type(x) for x in args])
+    return rx.container(
+        navbar(),
+        child,
+        rx.logo(),
+        rx.color_mode.button(position="bottom-left"),
+    )
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
+    return rx.base_page(
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+            rx.heading(State.label, size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
+            ),
+            rx.input(
+                default_value = State.label, 
+                on_change=State.handle_title_input_change
             ),
             rx.link(
                 rx.button("Check out our docs!"),
@@ -30,8 +127,7 @@ def index() -> rx.Component:
             spacing="5",
             justify="center",
             min_height="85vh",
-        ),
-        rx.logo(),
+        )
     )
 
 
